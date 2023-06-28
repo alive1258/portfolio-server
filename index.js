9438c732344bc7c51434afeb8d1ff0534e4c8b38
alive1258 <javashazib@gmail.com>
2023-06-28 11:03:29 +0600

    project done

--- index.js
+++ index.js
@@ -9,6 +9,45 @@ app.use(cors());
 app.use(express.json());
 
 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1yov7jc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const projectCollection = client.db("portfolioDB").collection("projects");
+
+    app.get('/projects',async(req,res)=>{
+      const result=await projectCollection.find().toArray()
+      res.send(result)
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get("/", (req, res) => {
     res.send("Portfolio is Making");
   });
