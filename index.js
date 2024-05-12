bb81c5a732977e0c9f25e5bac6d1fe3b951b4fdd
alive1258 <javashazib@gmail.com>
2024-05-12 08:30:28 +0600

    skills crudoperation done

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ async function run() {
       .collection("experiences");
     const blogCollection = client.db("portfolioDB").collection("blogs");
 
+    // Add projects
+    app.post("/api/v1/projects", async (req, res) => {
+      const addProjects = req.body;
+      const result = await projectCollection.insertOne(addProjects);
+      res.send(result);
+    });
     // get all projects
     app.get("/api/v1/projects", async (req, res) => {
       const result = await projectCollection.find().toArray();
@@ -56,8 +62,12 @@ async function run() {
       const options = { upsert: true };
       const project = {
         $set: {
-          title: updatedProject.title,
-          quantity: updatedProject.quantity,
+          title: updatedProject.name,
+          tec1: updatedProject.tec1,
+          tec2: updatedProject.tec2,
+          tec3: updatedProject.tec3,
+          rating: updatedProject.rating,
+          review: updatedProject.review,
           category: updatedProject.category,
         },
       };
@@ -78,9 +88,58 @@ async function run() {
       res.send(result);
     });
 
-    // skills
+    // Add skills
+    app.post("/api/v1/skills", async (req, res) => {
+      const addSkills = req.body;
+      const result = await skillsCollection.insertOne(addSkills);
+      res.send(result);
+    });
+
+    // get all skills
     app.get("/api/v1/skills", async (req, res) => {
       const result = await skillsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get a single skills by its ID
+    app.get("/api/v1/skills/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await skillsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // update skills
+    app.put("/api/v1/skills/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedSkill = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const skill = {
+        $set: {
+          title: updatedSkill.title,
+          quantity: updatedSkill.quantity,
+          category: updatedSkill.category,
+        },
+      };
+      const result = await skillsCollection.updateOne(filter, skill, options);
+
+      res.send(result);
+    });
+
+    //delete skills
+    app.delete("/api/v1/skills/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await skillsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Add educations
+    app.post("/api/v1/educations", async (req, res) => {
+      const addEducations = req.body;
+      const result = await educationCollection.insertOne(addEducations);
       res.send(result);
     });
     // educations
@@ -88,12 +147,104 @@ async function run() {
       const result = await educationCollection.find().toArray();
       res.send(result);
     });
+
+    // get a single educations by its ID
+    app.get("/api/v1/educations/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await educationCollection.findOne(query);
+      res.send(result);
+    });
+
+    // update educations
+    app.put("/api/v1/educations/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedEducation = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const education = {
+        $set: {
+          title: updatedEducation.title,
+          quantity: updatedEducation.quantity,
+          category: updatedEducation.category,
+        },
+      };
+      const result = await educationCollection.updateOne(
+        filter,
+        education,
+        options
+      );
+
+      res.send(result);
+    });
+
+    //delete educations
+    app.delete("/api/v1/educations/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await educationCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Add experiences
+    app.post("/api/v1/experiences", async (req, res) => {
+      const addExperiences = req.body;
+      const result = await experienceCollection.insertOne(addExperiences);
+      res.send(result);
+    });
     // experiences
     app.get("/api/v1/experiences", async (req, res) => {
       const result = await experienceCollection.find().toArray();
       res.send(result);
     });
-    // blogCollection
+
+    // get a single experiences by its ID
+    app.get("/api/v1/experiences/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await experienceCollection.findOne(query);
+      res.send(result);
+    });
+
+    // update experiences
+    app.put("/api/v1/experiences/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedExperiences = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const experience = {
+        $set: {
+          title: updatedExperiences.title,
+          quantity: updatedExperiences.quantity,
+          category: updatedExperiences.category,
+        },
+      };
+      const result = await experienceCollection.updateOne(
+        filter,
+        experience,
+        options
+      );
+
+      res.send(result);
+    });
+
+    //delete experiences
+    app.delete("/api/v1/experiences/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await experienceCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Add blogs
+    app.post("/api/v1/blogs", async (req, res) => {
+      const addBlogs = req.body;
+      const result = await blogCollection.insertOne(addBlogs);
+      res.send(result);
+    });
+    // get all blogs
     app.get("/api/v1/blogs", async (req, res) => {
       const result = await blogCollection.find().toArray();
       res.send(result);
@@ -103,6 +254,33 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await blogCollection.findOne(query);
+      res.send(result);
+    });
+
+    // update blogs
+    app.put("/api/v1/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedBlog = req.body;
+
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const blog = {
+        $set: {
+          title: updatedBlog.title,
+          quantity: updatedBlog.quantity,
+          category: updatedBlog.category,
+        },
+      };
+      const result = await blogCollection.updateOne(filter, blog, options);
+
+      res.send(result);
+    });
+
+    //delete blogs
+    app.delete("/api/v1/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogCollection.deleteOne(query);
       res.send(result);
     });
 
